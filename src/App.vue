ed349bc4ec60afc57f76c78d763c14af7023a1e6
Brian Hogg <brian@bhconsulting.ca>
2023-03-05 15:45:08 +0100

    Adding tailwind CSS, and adding some (beautiful) styling to the Vue app to test

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,14 @@ const { __, _x, _n, _nx } = wp.i18n
 <template>
   <header>
     <div class="wrapper">
-      {{ __('Events Calendar:', 'event-calendar-newsletter') }}
-      <select name="event_calendar">
-        <option value="the-events-calendar">The Events Calendar</option>
-      </select>
+      <div class="text-3xl italic text-red-400">
+        {{ __('Events Calendar:', 'event-calendar-newsletter') }}
+      </div>
+      <div>
+        <select name="event_calendar">
+          <option value="the-events-calendar">The Events Calendar</option>
+        </select>
+      </div>
     </div>
   </header>
 
